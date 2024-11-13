2acee93084b8b2469df38e245b2e73a755f637a7
Rajlamich <rajlamichhane.8284@gmail.com>
2024-11-13 12:59:41 +0200

    Replaced weight by mileage and some other updates. By Raj Lamichhane

--- groupWork.java
+++ groupWork.java
@@ -4,33 +4,36 @@ public class Car{
 
         public String brand;
         public String model;
-        public Float weight;
+        public Float minMileage;
         
         public Car()
         {
          brand="";
          model="";
-         weight=0.0f;
+         minMilage=0.0f;
         }
 
 
-        public Car(String brand, String model,Float weight)
+        public Car(String brand, String model,Float minMileage)
       {
         this.brand=brand;
         this.model=model;
-        this.weight=weight;
+        this.milage=minMileage;
       }
       public void displayInfo()
        {
         System.out.println("car brand:"+brand);
         System.out.println("model:"+model);
-        System.out.println("weight:"+weight);
+        System.out.println("minMilage:"+minMileage);
        }
        public static void main(String[] args) { 
-        Car car1= new Car("Toyota","Toyota Supra",2460f);
-        Car car2= new Car("Tesla", "Tesla Model Y",4209f); 
+        Car car1= new Car("Toyota","Toyota Supra",13.2);
+        Car car2= new Car("Tesla", "Tesla Model Y",393); 
+        Car car3= new Car("BMW","BMW i3",130);
         System.out.println("Car1 information:");
         car1.displayInfo();
         System.out.println("Car2 information");
         car2.displayInfo();
+        System.out.println("Car3 information:");
+        car3.displayInfo();
       }}
