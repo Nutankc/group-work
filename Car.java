049b5591cada211cd1f7ec4211a1299228c116b3
Puran11462 <amk1002351@student.hamk.fi>
2024-12-05 12:17:22 +0200

    Added method to calculate car age based on the manufacture year.

--- Car.java
+++ Car.java
@@ -1,39 +1,53 @@
-public class Car{
-    
-   //end of class Car
+public class Car {
 
-        public String brand;
-        public String model;
-        public int minMileage;
-        
-        public Car()
-        {
-         brand="";
-         model="";
-         minMileage=0;
-        }
+  // end of class Car
 
+  public String brand;
+  public String model;
+  public int minMileage;
 
-        public Car(String brand, String model,int minMileage)
-      {
-        this.brand=brand;
-        this.model=model;
-        this.minMileage=minMileage;
-      }
-      public void displayInfo()
-       {
-        System.out.println("car brand: "+brand);
-        System.out.println("model: "+model);
-        System.out.println("minMilage: "+minMileage+" km");
-       }
-       public static void main(String[] args) { 
-        Car car1= new Car("Toyota","Toyota Supra",13);
-        Car car2= new Car("Tesla", "Tesla Model Y",393); 
-        Car car3= new Car("BMW","BMW i3",130);
-        System.out.println("Car1 information:");
-        car1.displayInfo();
-        System.out.println("Car2 information");
-        car2.displayInfo();
-        System.out.println("Car3 information:");
-        car3.displayInfo();
-      }}
+  public Car() {
+    brand = "";
+    model = "";
+    minMileage = 0;
+  }
+
+  public Car(String brand, String model, int minMileage) {
+    this.brand = brand;
+    this.model = model;
+    this.minMileage = minMileage;
+  }
+
+  public void displayInfo() {
+    System.out.println("car brand: " + brand);
+    System.out.println("model: " + model);
+    System.out.println("minMilage: " + minMileage + " km");
+  }
+
+  // New method to calculate car age based on manufacture year
+  public int calculateCarAge(int manufactureYear) {
+    int currentYear = 2024;
+    return currentYear - manufactureYear;
+  }
+
+  public static void main(String[] args) {
+    Car car1 = new Car("Toyota", "Toyota Supra", 13);
+    Car car2 = new Car("Tesla", "Tesla Model Y", 393);
+    Car car3 = new Car("BMW", "BMW i3", 130);
+
+    System.out.println("Car1 information:");
+    car1.displayInfo();
+    int car1Age = car1.calculateCarAge(1998);
+    System.out.println("Car age: " + car1Age + " years\n");
+
+    System.out.println("Car2 information");
+    car2.displayInfo();
+    int car2Age = car2.calculateCarAge(2021);
+    System.out.println("Car age: " + car2Age + " years\n");
+
+    System.out.println("Car3 information:");
+    car3.displayInfo();
+    int car3Age = car3.calculateCarAge(2015);
+    System.out.println("Car age: " + car3Age + " years\n");
+  }
+}
